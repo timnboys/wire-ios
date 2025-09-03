517537c1ae40c3b6432ca2e7be62075f377ae399
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-09-03 16:01:08 +0200

    Introduce WireCellsLocalAssetStore

--- WireMessaging/Sources/WireMessagingAssembly/WireCellsFactory.swift
+++ WireMessaging/Sources/WireMessagingAssembly/WireCellsFactory.swift
@@ -30,6 +30,7 @@ public struct WireCellsFactory {
     private let fileCache: any FileCache
     private let localAssetRepository: WireCellsLocalAssetRepository
 
+    @MainActor
     public init(serverURL: URL, accessToken: any AccessTokenProvider, fileCache: any FileCache) {
         // TODO: [WPB-18798] Remove serverURL temporary override when there exists a method to obtain the correct URL.
         let serverURL = switch serverURL.host {
@@ -48,7 +49,7 @@ public struct WireCellsFactory {
         self.localAssetRepository = WireCellsLocalAssetRepository(
             nodesAPI: nodesAPI,
             fileCache: fileCache,
-            store: FakeWireCellsLocalAssetMetadataStore()
+            store: WireCellsLocalAssetStore()
         )
     }
 
